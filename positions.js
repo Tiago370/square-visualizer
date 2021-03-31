1f7eadf56aa4820e1902f074126f647151b229a9
Tiago370 <tiagoamral23@gmail.com>
2021-03-31 16:34:12 -0300

    inicio

---
+++ positions.js
@@ -0,0 +1,5 @@
+let bigSquare = 100
+let smallerSquare = 1
+let x = 99
+let y = 99
+let positions = [ 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L']
---
+++ sketch.js
@@ -0,0 +1,10 @@
+let screenSize = 600
+let c = screenSize/bigSquare
+function setup() {
+  createCanvas(screenSize, screenSize);
+}
+
+function draw() {
+  fill(102, 0, 0);
+  rect(0, 0, width, height);
+}
