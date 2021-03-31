7b02a82d01d77540bd3466e75286ac2d1c0034e4
Tiago370 <tiagoamral23@gmail.com>
2021-03-31 16:57:13 -0300

    nova cara

--- positions.js
+++ positions.js
@@ -1,5 +1,5 @@
 let bigSquare = 100
 let smallerSquare = 1
-let x = 99
-let y = 99
+let x = 0
+let y = 0
 let positions = [ 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L', 'R', 'L']
--- sketch.js
+++ sketch.js
@@ -1,10 +1,27 @@
 let screenSize = 600
 let c = screenSize/bigSquare
+let smaller = c*smallerSquare
+let p = 0
+let X = x*c
+let Y = screenSize - (y*c +smaller);
 function setup() {
   createCanvas(screenSize, screenSize);
 }
 
 function draw() {
+  if(positions[p] == 'R'){
+    X+=c;
+    console.log("Direita");
+  }else if(positions[p] == 'U'){
+    Y-=c;
+    console.log("Pra cima");
+  }
+  p++;
   fill(102, 0, 0);
   rect(0, 0, width, height);
+  fill(114, 242, 75);
+  rect((screenSize-smaller)/2, (screenSize-smaller)/2, smaller, smaller);
+  fill(255, 10, 30);
+  rect(X, Y, smaller, smaller);
+
 }
